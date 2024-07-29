c6f8a90ac0ec1a7ddcb2b3f88d2d09ba59e941cc
Thakisis <sub722@gmail.com>
2024-07-29 19:06:10 +0200

    removed data-props

--- src/components/Tools/ToolsList.jsx
+++ src/components/Tools/ToolsList.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { getData } from '@/server/getData'
 import Card from './Card'
-async function ToolsList({ tools = [] }) {
+function ToolsList({ tools = [] }) {
 
-    return tools.map((tool) => <Card key={tool.name} {...tool} tags={tool.tags} />)
+    return tools.map(({ tags, name, ...otherProps }) => <Card key={name} {...otherProps} name={name} tags={tags} />)
 }
 
 export default ToolsList
